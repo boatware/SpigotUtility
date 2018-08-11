ced5b97f89a012752e414b4871637d4abe0a4ac9
Thomas Artmann <thwohmas@googlemail.com>
2018-08-11 23:44:33 +0200

    Sicherung

--- src/de/exodus/fp/general/Config.java
+++ src/de/exodus/fp/general/Config.java
@@ -1,5 +1,7 @@
 package de.exodus.fp.general;
 
+import java.util.ArrayList;
+
 public class Config {
 	private boolean debug = true;
 	
---
+++ src/de/exodus/fp/helper/CommandLoader.java
@@ -0,0 +1,12 @@
+package de.exodus.fp.helper;
+
+public class CommandLoader {
+	public CommandLoader() {
+	
+	}
+	
+	public boolean loadCommands() {
+		
+		return true;
+	}
+}
