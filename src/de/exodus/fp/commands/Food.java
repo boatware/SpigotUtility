bbf7700e14b2cf1686aadc893db73118a4a71769
4xodus <42304783+4xodus@users.noreply.github.com>
2018-08-12 01:00:43 +0200

    New Command
    -food
    now you get Melon

--- src/de/exodus/fp/commands/Food.java
+++ src/de/exodus/fp/commands/Food.java
@@ -1,17 +1,20 @@
 package de.exodus.fp.commands;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class Food implements CommandExecutor{
-
+	//	ItemStack​(Material type, int amount)
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String string, String[] args) {
 		Player p = (Player) sender;
-		p.getInventory().getItem(1);
-		p.sendMessage("�a Du kannst nun essen!");
+		ItemStack stack = new ItemStack(Material.MELON, 5);
+		p.getInventory().addItem(stack);
+		p.sendMessage("§a Du kannst nun essen!");
 		
 		return false;
 	}
